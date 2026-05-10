c865d672a76fb6cdfbbdbc7bc59c022b2ec9129d
Pasan Kalhara <kalharap84@gmail.com>
2026-05-11 00:46:35 +0530

    feat: add fillable properties and relationship for SaleItem model

--- app/Models/SaleItem.php
+++ app/Models/SaleItem.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SaleItem extends Model
 {
-    //
+    public $timestamps = false;
+
+    protected $fillable = ['sale_id', 'product_id', 'quantity', 'unit_price', 'subtotal'];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
