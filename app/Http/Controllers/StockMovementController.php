a7df52b61170b0e72aec86076ed7d1a2600f97fe
Pasan Kalhara <kalharap84@gmail.com>
2026-05-11 00:58:59 +0530

    feat: add model imports in StockMovementController for Product and StockMovement

--- app/Http/Controllers/StockMovementController.php
+++ app/Http/Controllers/StockMovementController.php
@@ -1,8 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Product;
+use App\Models\StockMovement;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class StockMovementController extends Controller
 {
