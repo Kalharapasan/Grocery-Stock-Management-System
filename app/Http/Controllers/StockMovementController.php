14bb1987ff0065c1800b818abaa131d614e3f912
Pasan Kalhara <kalharap84@gmail.com>
2026-05-11 01:05:32 +0530

    feat: implement stock movement creation in stockIn method of StockMovementController

--- app/Http/Controllers/StockMovementController.php
+++ app/Http/Controllers/StockMovementController.php
@@ -17,7 +17,14 @@ class StockMovementController extends Controller
         ]);
 
         DB::transaction(function () use ($request, $product) {
-            
+            StockMovement::create([
+                'product_id' => $product->id,
+                'user_id' => Auth::id(),
+                'type' => 'in',
+                'quantity' => $request->quantity,
+                'reference' => $request->reference,
+                'notes' => $request->notes,
+            ]);
         });
     }
 }
