e781745e670865a62df94c62c58d170b8c5b1014
Pasan Kalhara <kalharap84@gmail.com>
2026-05-11 00:55:36 +0530

    feat: implement category retrieval in create method of ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -32,7 +32,8 @@ class ProductController extends Controller
 
     public function create()
     {
-        //
+        $categories = Category::all();
+        return view('products.create', compact('categories'));
     }
 
 
