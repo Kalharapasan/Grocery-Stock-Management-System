98382a425d25e8a746b76bbf3aea8382d8fbecce
Pasan Kalhara <kalharap84@gmail.com>
2026-05-12 00:28:53 +0530

    feat: add total amount to sale creation in store method of SaleController

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -48,6 +48,15 @@ class SaleController extends Controller
                 $total += $subtotal;
                 $lines[] = compact('product', 'item', 'subtotal');
             }
+
+            $sale = Sale::create([
+                'user_id' => Auth::id(),
+                'customer_name' => $request->customer_name,
+                'customer_phone' => $request->customer_phone,
+                'total_amount' => $total,
+                'notes' => $request->notes,
+            ]);
+
         });
 
     }
